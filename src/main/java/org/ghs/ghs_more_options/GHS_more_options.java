27e2f39c478f3be2eee9baea5bbf37a7421040d6
mc_doge_ <wangyaocing@outlook.com>
2024-02-01 20:09:38 +0800

    Update client,main

--- src/main/java/org/ghs/ghs_more_options/GHS_more_options.java
+++ src/main/java/org/ghs/ghs_more_options/GHS_more_options.java
@@ -1,2 +1,23 @@
-public class GHS_more_options {
+package org.ghs.ghs_more_options;
+
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GHS_more_options implements ModInitializer {
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+        System.out.println("[GHS] mod成功加载");
+    }
 }
--- src/main/java/org/ghs/ghs_more_options/client/GHS_more_optionsClient.java
+++ src/main/java/org/ghs/ghs_more_options/client/GHS_more_optionsClient.java
@@ -1,2 +1,10 @@
-public class GHS_more_optionsClient {
+package org.ghs.ghs_more_options.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GHS_more_optionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
